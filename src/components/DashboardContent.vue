21d3f523a7d60dcf106f24127020ba5e6e9f8528
ian <iandavis6481@gmail.com>
2024-05-08 07:26:59 -0500

    user table update

--- src/components/DashboardContent.vue
+++ src/components/DashboardContent.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="flex justify-center p-4">
-    <div class="max-w-screen-2xl grid grid-cols-3 gap-4">
+    <div class="max-w-screen-2xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       <!-- START: Row 1 -->
-      <user-table class="col-span-3"></user-table>
+      <user-table class="col-span-1 sm:col-span-2 md:col-span-3"></user-table>
       <!-- END: Row 1 -->
 
       <!-- START: Row 2 -->
 
       <basic-image-card
+        class="col-span-1"
         v-for="i in 3"
         :title="`Shoes ${i}!`"
         :body="'Check out these awesome shoes!'"
--- src/components/layout/SideNavLayout.vue
+++ src/components/layout/SideNavLayout.vue
@@ -2,16 +2,16 @@
   <div>
     <div class="drawer lg:drawer-open">
       <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content nav-max-h min-h-full bg-base-200">
+      <div class="drawer-content nav-max-h min-h-full">
         <!-- Page content here -->
         <label for="my-drawer" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
         <slot name="page-content"></slot>
       </div>
 
-      <div class="drawer-side nav-max-h bg-base-200">
+      <div class="drawer-side overflow-y-auto min-h-full nav-max-h">
         <label for="my-drawer" class="drawer-overlay" />
-        <ul class="menu p-4 overflow-y-auto w-80 min-h-full">
+        <ul class="menu w-80 p-4 bg-base-100 min-h-full">
           <!-- Sidebar Content here -->
           <slot name="nav-content"></slot>
         </ul>
