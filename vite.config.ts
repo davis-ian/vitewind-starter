e037353e501a9b4eb5cbafdfad403f7797489bed
ian <iandavis6481@gmail.com>
2024-05-10 19:05:06 -0500

    revert daisyui config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
